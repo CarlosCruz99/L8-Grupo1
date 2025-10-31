aac06500bcf9bc690322d2a345da0fa976c31fb9
CarlosCruz99 <00013324@uca.edu.sv>
2025-10-31 10:36:19 -0600

    docs: Agregar explicación de como se obtuvo la contraseña

--- 00013324_practica8_seccion1/Server-BE/App.js
+++ 00013324_practica8_seccion1/Server-BE/App.js
@@ -35,6 +35,14 @@ app.delete('/users/:id', verifyToken, controllers.deleteUser)
 app.post("/signIn", async (req, res) => {
   const { email, password } = req.body;
   var stored_hash = '$2b$10$XOHI0.vg73ve9Oy73lqU5.W//tVQuqMIAKJn3n25ZA2DUF.YGm6vq'
+
+  // En el momento que empecé a realizar la guía, habían varias contraseñas sin hash,
+  // fuí comparando cada una de las contraseñas para saber cual era, encontrando que la contraseña
+  // era 1234, aunque no había un usuario Jerry con esa contraseña.
+
+  // Por lo que, al ingresar un usuario con esa contraseña en el Postman,
+  // se puede verificar que es la misma de la variable stored_hash.
+
   if (await bcrypt.compare(password, stored_hash)) {
     console.log("La contraseña es correcta")
   }
